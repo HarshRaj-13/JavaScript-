4f012c872b473120eb605774620d71339d033e5e
HARSH RAJ <harshrajssm130102@gmail.com>
2023-10-30 07:17:53 +0000

    datatypes

---
+++ Basic_JS/1_variables.js
@@ -0,0 +1,19 @@
+const accId = 14453
+let accEmail = "harshraj@gmail.com"
+var accPassword = "12345"
+accCity = "jaipur" // although this is possible but its not proper way of writing as its ruins code readibility.
+
+// accId = 4321 --> Assignment to const is not possible.
+
+/*The difference between let and var is in the scope of the variables they create: Variables declared by let are only available inside the block where they're defined. Variables declared by var are available throughout the function in which they're declared.
+✅ therefore prefer not to use var
+*/
+accEmail = "harshrajssm13@gmail.com"
+accPassword = "54321"
+accCity = "Bengaluru"
+console.log(accId)
+
+
+console.table([accId,accEmail,accPassword,accCity])
+
+
---
+++ Basic_JS/2_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // it means treat all JS code as newer version .
+
+// alert(3+3) --> we are using nodejs not console browser
+
+let name = "Harsh" // String
+let age = 19 // int
+let flag = true // boolean
+
+//🔥 null is a standalone value
+
+let investigate // Undefined means variable is declared but value is not defined
+let investigate2 = null // null is a representation of empty value.
+
+console.table([investigate,investigate2])
+
+console.log("Type of Null is : " + typeof null + "🤔")
+
+/*➡️ symbol --> JavaScript Symbols are a new type of primitive data type introduced in the ES6 version of the language. They are used to represent unique values that can be used as identifiers or keys in objects. They are also used to create private properties and methods in classes.
+        A symbol is a unique and immutable data type and may be used as an identifier for object properties. 
+*/
+
+console.log(typeof "Harsh")
+console.log(typeof 12)
+console.log(typeof true)
+console.log("Type of null is :" + typeof null + "🔥")
