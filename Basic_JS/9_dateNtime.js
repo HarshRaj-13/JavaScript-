e90aa905a3226e32ef0ae8bd50ad70377aa56eed
HARSH RAJ <harshrajssm130102@gmail.com>
2023-11-01 18:12:50 +0000

    array

--- Basic_JS/9_dateNtime.js
+++ Basic_JS/9_dateNtime.js
@@ -11,12 +11,36 @@ console.log(myDate.toLocaleString());      // 11/1/2023, 9:38:06 AM
 console.log(myDate.toLocaleDateString());  // 11/1/2023
 console.log(myDate.toJSON());              // 2023-11-01T09:38:06.629Z
 
+console.log("-------------------------------------------")
 
-let myCreatedDate = new Date (2023, 0, 13) // jan-0 to dec-11
-let myCreatedDate2 = new Date (2023, 0, 13 , 3, 5) 
-console.log(myCreatedDate. toDateString())
-console.log(myCreatedDate2. toLocaleString())
+// let myCreatedDate = new Date (2023, 0, 13) // jan-0 to dec-11
+// let myCreatedDate2 = new Date (2023, 0, 13 , 3, 5) 
+// console.log(myCreatedDate. toDateString())
+// console.log(myCreatedDate2. toLocaleString())
 
+// let myCreatedDate = new Date(2023, 0, 23)
+ // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+console. log (myCreatedDate. toLocaleString());
+
+console.log("-----------TIME STAMP------------");
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp); // output is very big number which is in milliseconds from year 1970
+// console.log(myCreatedDate.getTime()) // output : big value
+ console. log (Math. floor (Date. now()/1000));// to deal in seconds {IMP for INTerviews}
+
+ let newDate = new Date()
+ console.log(newDate)
+ console.log(newDate.getMonth() + 1);
+ console.log(newDate.getDay());
+ 
+// below one is imp for interview
+
+newDate.toLocaleString('default' , {
+    weekday:"long",
+})
 
 
 
---
+++ Basic_JS_2/1_Array.js
@@ -0,0 +1,15 @@
+// () --> parenthesis
+// [] --> brackets
+// {} --> curly braces
+
+/*The Array object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.
+
+JavaScript arrays are resizable and can contain a mix of different data types. 
+
+
+INTERVIEWS IMP : JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).
+
+A shallow copy of an object is a copy whose properties share the same references.
+
+A deep copy of an object is a copy whose properties do not share the same references.
+*/
