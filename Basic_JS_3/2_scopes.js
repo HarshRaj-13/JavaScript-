e439272d2b96906d7659fcfec80debc2c244c075
HARSH RAJ <harshrajssm130102@gmail.com>
2023-11-06 08:55:05 +0000

    ScopeAdded

--- Basic_JS_3/2_scopes.js
+++ Basic_JS_3/2_scopes.js
@@ -13,3 +13,11 @@ if(true){
 // console.log(b); -->  The scope of a const variable is block scope.
 console.log(c);
 console.log(a);
+
+// Before ES6 (2015), JavaScript had only Global Scope and Function Scope.
+
+// ES6 introduced two important new JavaScript keywords: let and const.
+
+// These two keywords provide Block Scope in JavaScript.
+
+// Variables declared inside a { } block cannot be accessed from outside the block:
