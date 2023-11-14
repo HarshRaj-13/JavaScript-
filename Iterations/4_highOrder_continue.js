8cec516b65b92e79f60139755b4e214ad1b17eae
HARSH RAJ <harshrajssm130102@gmail.com>
2023-11-14 20:35:07 +0000

    Higher Order Loops

---
+++ Iterations/4_highOrder_continue.js
@@ -0,0 +1,12 @@
+// iteration of objects using for in loop
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(`${key} refers to ${myObject[key]}`);
+}
